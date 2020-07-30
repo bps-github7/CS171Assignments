02c06a0f722a27601a5d529b7fddcca662b920d9
unknown <bps53@drexel.edu>
2020-07-30 09:41:48 -0400

    PEP8 and descriptor protocol compliance
    edited the Socialite class to make the class more
    up to date, readable and pythonic.
    Added some basic tests to debug.
    Everything is working as expected

--- Socialite.py
+++ Socialite.py
@@ -6,39 +6,25 @@
 class Socialite:
 
     #initializer method
-    def __init__(self):
-        self.__lastName = ''
-        self.__firstName = ''
-        self.__picture = ''
-        self.__website = ''
-        self.__description = ''
-        self.__userID = ''
-    
-    #Mutator Methods
-    def setLastName(self, lastName):
-        self.__lastName = lastName
-        
-    def setFirstName(self, firstName):
-        self.__firstName = firstName
-    
-    def setPicture(self, picture):
-        self.__picture = picture
-    
-    def setWebsite(self, website):
-        self.__website = website
-    
-    def setDescription(self, description):
-        self.__description = description
-    
-    def setUserID(self, userID):
-        self.__userID = userID
+    def __init__(self, first, last, pic, web, desc, uID):
+        self._firstName = first
+        self._lastName = last
+        self._picture = pic
+        self._website = web
+        self._description = desc
+        self._userID = uID
 
-    #Accessor Methods
-    def getLastName(self):
-        return self.__lastName
-    
-    def getFirstName(self):
-        return self._firstName
-    
-    def getPicture(self):
-        return self._picture
+    def __str__(self):
+        return "name: {} {}\nPicture: {}\nWebsite: {}\nDescription: {}\nUserID: {}".format(self._firstName, self._lastName, self._picture, self._website, self._description, self._userID)
+
+if __name__ == "__main__":
+    test = Socialite('Ben', 'Sehnert', 'https://no.com', 'apricot.com', 'a picture of me eating a grapefruit', 12345)
+
+    print(test.__str__())
+    print("\n\n")
+    print(test._description)
+    print("\n\n")
+
+    test._description = "no frogs allowed in this outhouse!"
+
+    print(test.__str__())
