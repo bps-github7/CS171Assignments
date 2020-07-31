1c9107d66477433e812a46ce1a1ad8468672ad63
Ben Sehnert <bps53@protonmail.com>
2020-07-31 06:53:16 -0400

    Updated sum and quotient functions in Equality.py
    improved main function
    Now cooperates with parsing less than ideal cmd line args

--- Complex/Equality.py
+++ Complex/Equality.py
@@ -3,6 +3,27 @@
 ### Date: 7/29/18
 ### Software: Complex Number Calculator
 
+"""
+Equality.py script
+
+takes 3 arguments, first two floats and last an integer
+
+caluclates a summation using Complex numbers:
+
+sigma (0,k) sum=0, num=0, n+1 : sum += Complex(2.0, 3.0) ^ num
+where the real and imaginary arguments to Complex constructor 
+are overrideen by the first and second cmd line arguments (if provided).
+
+quoitent: 
+
+(Complex(1.0, 0) - c ** (k + 1))/(Complex(1.0, 0) - c)
+
+Developer notes:
+
+Reaches recursion depth limit when k reaches 1000 
+(993 exactly), for unknown reasons.
+"""
+
 from Complex import Complex
 import sys
 
@@ -10,27 +31,31 @@ def equality(real = 2.0, imaginary = 3.0, k = 10):
     c = Complex(real, imaginary)
 
     def summation(c,k):
-        '''left side of complex number summation'''
         sum, num = Complex(0), 0
-        while num <= k: 
-            sum += c ** num 
-            num += 1
+        for i in range(num, k+1): sum += c ** i
         return sum
 
-    def quotient(c,k):
-        '''right side of complex number summation'''
-        one = Complex(1.0,0)
-        numerator = one - c ** (k + 1)
-        denominator = one - c
+    def quotient(c,k, parens = Complex(1.0,0)):
+        numerator = parens - c ** (k + 1)
+        denominator = parens - c
         return numerator / denominator
 
     return True if str(summation(c,k)) == str(quotient(c,k)) else False
 
 def main():
-    print(equality(real = float(sys.argv[1]), imaginary = float(sys.argv[2]),
-    k = float(sys.argv[3])) if len(sys.argv) > 2 else\
-    "must pass in all three arguments or none"\
-    if (len(sys.argv) < 2 and len(sys.argv) > 1) else equality())
+    #compound ternary was getting hard to debug and configure
+    if len(sys.argv) == 1:
+        print(equality())
+        return 1
+    try:
+        real, imaginary, k = float(sys.argv[1]), float(sys.argv[2]), int(sys.argv[3])
+    except IndexError:
+        print("must pass in all three arguments or none.")
+        return 0
+    # Checks if these variables/ arguments are defined
+    if (real, imaginary, k):
+        print(equality(real, imaginary, k))
+        return 1
 
 if __name__ == '__main__':
     main()
