68f7637a0f7fa740728ad19eecfb32e2dd36dffc
Ben Sehnert <bps53@protonmail.com>
2020-07-30 20:34:46 -0400

    Docstrings and pep8 compliance in Complex Class

--- Complex/Complex.py
+++ Complex/Complex.py
@@ -1,15 +1,22 @@
-###Programmer: Ben Sehnert
-###Program: Complex number class
-###
-###
+### Programmer: Ben Sehnert
+### Program: Complex number class
+### Date: 7/30/2020
+### Software: Complex Number class
 
 '''
-Module Level Docstring
+Module Level Docstring: Implements a Complex number class
+with overridden magic methods for addition, subtraction,
+multiplication and division.
 '''
 
 class Complex:
     '''
-    Class level docstring:
+    Class level docstring: Generates a Complex number
+    capable of doing basic arithmetic up to exponentiation
+    given a real and imaginary number as arguments to the constructor
+
+    surrounds negative numbers with parenthesis 
+    and rounds decimals to 4 significant digits.
     '''
     version = 2.1
 
@@ -42,15 +49,13 @@ class Complex:
     def __truediv__(self, other):
         return Complex((self.real * other.real +\
         self.imaginary * other.imaginary) /\
-        ((other.real ** 2)+(other.imaginary ** 2)),
+        ((other.real ** 2) + (other.imaginary ** 2)),
         (self.imaginary * other.real - self.real * other.imaginary)\
-        / ((other.real ** 2)+(other.imaginary ** 2)))
+        / ((other.real ** 2) + (other.imaginary ** 2)))
 
     def __pow__(self, other):
         return Complex(1,0) if other == 0 else self\
         if other == 1 else (self * self ** (other - 1))
-
-#can we pass in c1 and c2 as keyword args or args
 
 if __name__ == '__main__':
     print("Running in standby mode")
