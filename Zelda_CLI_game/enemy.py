f71397404779f2300a47f01c4f39a580ce8c2f62
Ben Sehnert <bps53@protonmail.com>
2020-08-01 16:04:41 -0400

    Updated 'enemy' Abstract Base Class.
    All contained code has been modified to be PEP8 Compliant
    
    NOTE: other classes and scripts in this directory will need refactored
    In paticular, all abstract methods had their signitures modified to
    all
    lowercase_underscore seperated case.
    
    file comments were transfered to method docstring.
    
    added base class 'object' as a mix-in. Was not sure if this
    is appropriate/ the thing to do, but seems logical
    
    left 3 accessor methods which will increase reusability
    by helping the classes users undertand how gameplay mechanics work.

--- Zelda_CLI_game/enemy.py
+++ Zelda_CLI_game/enemy.py
@@ -1,55 +1,107 @@
-##Ben Sehnert
-##8/15/18
-##CS172-HW3 'enemy' abstract base class
+'''
+Programmer: Ben Sehnert
+Program: Enemy Abstract base class
+Date: 8/1/2020
+Software: Zelda CLI mini-game.
 
-#import statement #imports ABC module for creation of 'enemy' ABC
+Module Level Docstring: Implements an 'enemy' abstract base class (ABC)
+which provides the structure and documentation for all in-game objects.
+'''
+
 import abc
 
-class enemy(metaclass = abc.ABCMeta):
-    #init and str methods left empty. Must be initialized by child classes
+class enemy(metaclass = abc.ABCMeta, object):
+    '''Class Level Docstring: creates signitures/ abstract methods
+    for init (constructor), __str__ (string repr.) and all helper
+    methods for gameplay.
+
+    NOTE: getter methods are retained from version 1, despite
+    this class adhering to the Descriptor protocol/ being a 
+    new style classe.
+    
+    This is done in order to facilitate documentation. 
+    When users implement the enemy class they will understand 
+    how use of their object attributes makes gameplay/ battle possible.
+    '''
+
+    version = 2.0
+
+    @abc._abc_init
     def __init__(self):
+        '''Construct enemy by passing in name, desc & health as args.'''
         pass
+    
+    @abc.abstractmethod
     def __str__(self):
-        pass
-    #abstract methods for accessor/mutator methods
+        '''Returns all attributes/properties of the enemy.'''
+        return NotImplemented
+
+    @property
     @abc.abstractmethod
     def getName(self):
-        pass
+        '''Returns the enemy name.'''
+        return NotImplemented
+    
+    @property
     @abc.abstractmethod
     def getDescription(self):
-        pass
+        '''Returns a short message describing
+    the enemy and providing any nessecary information.
+        '''
+        return NotImplemented
+    
+    @property
     @abc.abstractmethod
-    def getHealth(self): #enemy with health zero is unconcsious
-        pass
-    @abc.abstractmethod#used by the other enemy
-    def doDamage(self, damage):#positive int = damage, negative int = heal
-        pass
-    #Reset Health for next match
+    def getHealth(self):
+        '''An enemy with 0 health is defeated.'''
+        return NotImplemented
+    
+    #gamified versions of setters/mutator methods
+
     @abc.abstractmethod
-    def resetHealth(self):
-        pass
-#enemy moveset:  basic attack move, defense move, special attack move
-    #You are passed the monster you are fighting
+    def doDamage(self, damage):
+        '''Positive numeric argument does damage,
+        and negative numeric argument heals enemy.'''
+        return NotImplemented
+    
     @abc.abstractmethod
-    def basicAttack(self,enemy):
-        pass	
-    #Print the name of the attack used
+    def reset_health(self):
+        '''Returns health to starting HP.'''
+        return NotImplemented
+
+    # the attacks include: basic attack, defensive attack, special attack
+    # each of these has a corresponding method for returning the attack name.  
+    
     @abc.abstractmethod
-    def basicName(self):
-        pass
-    #lets the monster defend itself
+    def basic_attack(self, enemy):
+        '''A less powerful, more commonly used attack.'''
+        return NotImplemented	
+    
     @abc.abstractmethod
-    def defenseAttack(self,enemy):
-        pass	
-    #Print out the name of the attack used
+    def basic_name(self):
+        '''returns basic attack name.'''
+        return NotImplemented
+
     @abc.abstractmethod
-    def defenseName(self):
-        pass	
-    #Special Attack
+    def defense_attack(self, enemy):
+        '''Defensive move that counters or blocks an opponent
+    attack. some may infrequently do damage to opponent.
+        '''
+        return NotImplemented
+
     @abc.abstractmethod
-    def specialAttack(self,enemy):
-        pass
-    #prints name of special attack
+    def defense_name(self):
+        '''Returns defense attack name.'''
+        return NotImplemented
+	
+    @abc.abstractmethod
+    def special_attack(self, enemy):
+        '''Special attack which has limited availability, that may deal 
+    extra damanage, have increased accurarcy, or some other desired effect.
+        '''
+        return NotImplemented
+    
     @abc.abstractmethod
     def specialName(self):
-        pass
+        '''Returns name of special attack,'''
+        return NotImplemented
